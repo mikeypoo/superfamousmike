340ed06be3d6d568f0045b3df48bffe5fc52424c
Mike McGowan <mmcgowan@everlane.com>
2020-10-16 23:14:41 -0700

    new fun thing

--- main/src/App.vue
+++ main/src/App.vue
@@ -1,45 +1,31 @@
 <template>
-  <div>
-    <site-header />
-    <intro-section />
-    <section-header>Listen to his <em>feelings</em> music</section-header>
-    <music-section />
-    <section-header>Look at pictures of his stupid face</section-header>
-    <photo-gallery />
-    <section-header>Follow him on his dumb social media things</section-header>
-    <social-media />
-    <site-footer />
+  <div class="app-root">
+    <mobile-base />
   </div>
 </template>
 
 <script>
-import SiteHeader from "./components/SiteHeader.vue";
-import IntroSection from "./components/IntroSection.vue";
-import SectionHeader from "./components/SectionHeader.vue";
-import MusicSection from "./components/MusicSection.vue";
-import PhotoGallery from "./components/PhotoGallery.vue";
-import SocialMedia from "./components/SocialMedia.vue";
-import SiteFooter from "./components/SiteFooter.vue";
+import MobileBase from "./components/MobileBase.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    SiteHeader, 
-    IntroSection, 
-    SectionHeader,
-    MusicSection, 
-    PhotoGallery, 
-    SocialMedia, 
-    SiteFooter },
-}
+    MobileBase,
+  },
+};
 </script>
 
 <style>
-  @import "./global.css";
-  body {
-    color: var(--super-famous-black);
-    background-color: var(--super-famous-white);
-    font-family: var(--super-famous-font);
-    margin: 0;
-  }
+@import "./global.css";
+body {
+  color: var(--super-famous-black);
+  background-color: var(--super-famous-white);
+  font-family: var(--super-famous-font);
+  margin: 0;
+}
+
+.app-root {
+  height: 100vh;
+  overflow: hidden;
+}
 </style>
---
+++ main/src/components/MobileBase.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="mobile-base">
+    <div
+      class="mobile-base__menu"
+      :key="menuItem.id"
+      @click="selectItem(menuItem.name)"
+      v-for="menuItem in menuItems">
+      <div class="mobile-base__menu-item">
+        <span>{{ menuItem.name }}</span>
+      </div>
+    </div>
+    <div class="mobile-base__bottom">
+      <div class="mobile-base__title">
+        <div>Super</div>
+        <div>Famous</div>
+        <div>Mike</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MobileBase",
+  data: () => {
+    return {
+      menuItems: [
+        { id: 1, name: "Music" },
+        { id: 2, name: "Handfarts" },
+        { id: 3, name: "Software" },
+      ],
+    };
+  },
+  methods: {
+    selectItem: function(item) {
+      console.log("ITEM CLICK", item);
+    },
+  },
+};
+</script>
+
+<style scoped>
+@import "../global.css";
+
+.mobile-base {
+  height: 100%;
+  background: var(--super-famous-black);
+  color: var(--super-famous-white);
+}
+
+.mobile-base__menu {
+  width: 100%;
+}
+
+.mobile-base__menu-item {
+  font-size: 64px;
+  width: 100%;
+  border-bottom: 2px solid white;
+  padding-top: 16px;
+  padding-left: 24px;
+}
+
+.mobile-base__title {
+  text-align: left;
+  margin-left: 64px;
+  font-size: 64px;
+  transform: rotate(-45deg);
+}
+</style>
